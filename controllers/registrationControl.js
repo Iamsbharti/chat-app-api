36f5ba19a81601d49971ade1a6e5bfb8a6b7954b
saurabh <saurabhbharti9@gmail.com>
2020-06-30 16:27:28 +0530

    added response format lib

--- controllers/registrationControl.js
+++ controllers/registrationControl.js
@@ -1,3 +1,4 @@
+const { response } = require("../lib");
 exports.test = (req, res) => {
-  res.send("Test Sucess, welcome to chat-app");
+  res.status(200).json(response(false, "Test Sucess", " welcome to chat-app"));
 };
