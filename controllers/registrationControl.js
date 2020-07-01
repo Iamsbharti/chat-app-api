255d9040ad6e3e315c8b9b5434a49634a6ea5515
saurabh <saurabhbharti9@gmail.com>
2020-07-01 09:13:47 +0530

    implemented create user func

--- controllers/registrationControl.js
+++ controllers/registrationControl.js
@@ -1,5 +1,7 @@
-const { response } = require("../lib");
+const { response, hashedPassword } = require("../lib");
 const { ValidEmail, ValidPassword } = require("../lib/paramsValidation");
+const User = require("../models/User");
+
 exports.test = (req, res) => {
   res.status(200).json(response(false, "Test Sucess", " welcome to chat-app"));
 };
@@ -20,10 +22,24 @@ exports.register = async (req, res) => {
     return result;
   };
   //store to db
+
+  createUser = async () => {
+    let newUser = new User({
+      email: email,
+      password: await hashedPassword(password),
+    });
+    let createdUser = await User.create(newUser);
+    return response(false, "User Create Success", createUser);
+  };
+
   //send response
 
   try {
-    await validateInput().then(createUser);
+    await validateInput()
+      .then(createUser)
+      .then((result) => {
+        res.send(result);
+      });
   } catch (error) {
     console.warn(error);
     res.send(error);
