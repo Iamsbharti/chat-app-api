b0c7e9543c689b660e131af06e53826b7b5ae456
saurabh <saurabhbharti9@gmail.com>
2020-07-08 09:48:24 +0530

    added chat routes

---
+++ controllers/chatController.js
@@ -0,0 +1,1 @@
+exports.getChatForUser = (req, res) => {};
--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,15 @@ const { test, register } = require("../controllers/registrationControl");
 const { login } = require("../controllers/loginControl");
 const { getAllUsers } = require("../controllers/allUsersControl");
 const { isAuthorized } = require("../middlewares");
+const { getChatForUser } = require("../controllers/chatController");
 const router = express.Router();
 
+/**Authentication and Authorization */
 router.get("/test", test);
 router.post("/register", register);
 router.post("/login", login);
 router.get("/allUsers", isAuthorized, getAllUsers);
+
+/**Chat Routes */
+router.get("/get/for/user", isAuthorized, getChatForUser);
 module.exports = router;
